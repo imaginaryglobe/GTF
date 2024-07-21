a6681cfbd30e7392474e05e7a2f6c9efc39b2134
Jaden Fang <jadenfang9@gmail.com>
2024-07-21 09:46:09 -0400

    center window

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import random
 import json
 from PIL import Image, ImageTk
 
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
+    
 with open("reversed_countries.json") as file:
     data = json.load(file)
     COUNTRY_LIST = [country.lower() + ".png" for country in data]
@@ -12,18 +22,16 @@ def choose_flag():
     return f"images/{file_name}"
 
 def show_flag():
-    print("button clicked")
     flag_path = choose_flag()
-    print("flag path chosen")
     flag_picture = ImageTk.PhotoImage(Image.open(flag_path))
-    print("flag picture created")
     flag_label.config(image=flag_picture)
     flag_label.image = flag_picture
 
 window = tk.Tk()
 window.maxsize(400, 400)
 window.minsize(400, 400)
-window.geometry("400x400+1000+200")
+center_window(window)
+
 
 top_text = tk.Label(window, text="Guess the flag")
 top_text.pack(pady=10)  
